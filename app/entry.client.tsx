fb072cf286e26b93611ec4abdb0cb0d695b76067
arpitdalal <arpitdalalm@gmail.com>
2022-04-21 15:04:56 -0400

    ➕ add React 18

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,8 @@
-import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
+import { hydrateRoot } from "react-dom/client";
 
-hydrate(<RemixBrowser />, document);
+import { RemixBrowser } from "@remix-run/react";
+
+hydrateRoot(document, <RemixBrowser />);
 
 if ("serviceWorker" in navigator) {
   // Use the window load event to keep the page load performant
